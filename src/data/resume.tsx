6262b89861ab2bf7431b2299a64082212152029e
Amritpal Singh <35212838+amritpal7@users.noreply.github.com>
2025-07-10 11:48:24 +0530

    added new projects

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -87,66 +87,65 @@ export const DATA = {
     },
   ],
   projects: [
-    // {
-    //   title: "Chat Collect",
-    //   href: "https://chatcollect.com",
-    //   dates: "Jan 2024 - Feb 2024",
-    //   active: true,
-    //   description:
-    //     "With the release of the [OpenAI GPT Store](https://openai.com/blog/introducing-the-gpt-store), I decided to build a SaaS which allows users to collect email addresses from their GPT users. This is a great way to build an audience and monetize your GPT API usage.",
-    //   technologies: [
-    //     "Next.js",
-    //     "Typescript",
-    //     "PostgreSQL",
-    //     "Prisma",
-    //     "TailwindCSS",
-    //     "Stripe",
-    //     "Shadcn UI",
-    //     "Magic UI",
-    //   ],
-    //   links: [
-    //     {
-    //       type: "Website",
-    //       href: "https://chatcollect.com",
-    //       icon: <Icons.globe className="size-3" />,
-    //     },
-    //   ],
-    //   image: "",
-    //   video:
-    //     "https://pub-83c5db439b40468498f97946200806f7.r2.dev/chat-collect.mp4",
-    // },
-    // {
-    //   title: "Magic UI",
-    //   href: "https://magicui.design",
-    //   dates: "June 2023 - Present",
-    //   active: true,
-    //   description:
-    //     "Designed, developed and sold animated UI components for developers.",
-    //   technologies: [
-    //     "Next.js",
-    //     "Typescript",
-    //     "PostgreSQL",
-    //     "Prisma",
-    //     "TailwindCSS",
-    //     "Stripe",
-    //     "Shadcn UI",
-    //     "Magic UI",
-    //   ],
-    //   links: [
-    //     {
-    //       type: "Website",
-    //       href: "https://magicui.design",
-    //       icon: <Icons.globe className="size-3" />,
-    //     },
-    //     {
-    //       type: "Source",
-    //       href: "https://github.com/magicuidesign/magicui",
-    //       icon: <Icons.github className="size-3" />,
-    //     },
-    //   ],
-    //   image: "",
-    //   video: "https://cdn.magicui.design/bento-grid.mp4",
-    // },
+    {
+      title: "X-Shortener",
+      href: "",
+      dates: "Jan 2024 - Feb 2024",
+      active: true,
+      description:
+        "Just to trasfrom your original long bulky links to tiny small links",
+      technologies: [
+        "React.js",
+        "Typescript",
+        "PostgreSQL",
+        "Prisma",
+        "NodeJs",
+        "TailwindCSS",
+        "TanStack Query",
+        "Docker",
+        "Redis",
+        "Shadcn UI",
+        "bullMq",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "",
+          icon: <Icons.globe className="size-3" />,
+        },
+        {
+          type: "Source",
+          href: "https://github.com/amritpal7/mern-interactive-product-demo",
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      image: "/scalerX.png",
+      video:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/chat-collect.mp4",
+    },
+    {
+      title: "Shop on top",
+      href: "https://magicui.design",
+      dates: "June 2023 - Present",
+      active: true,
+      description:
+        "Designed and developed using mock Dummy Json with fake data.",
+      technologies: ["React.js", "Typescript", "Dummy JSON", "TailwindCSS"],
+      links: [
+        {
+          type: "Website",
+          href: "https://vite-re-dummy.vercel.app/",
+          icon: <Icons.globe className="size-3" />,
+        },
+        {
+          type: "Source",
+          href: "https://github.com/amritpal7/vite-re-dummy",
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      image: "/Shop.png",
+      video: "https://cdn.magicui.design/bento-grid.mp4",
+    },
     {
       title: "Firebase Auth (In progress...)",
       href: "",
